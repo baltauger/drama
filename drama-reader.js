6bf3b07610ad7cb05d86b346d13d7966e23b5cfa
Balthazar Auger <balthazar.auger@gmail.com>
2019-02-18 02:35:19 -0300

    progress on replacing the word splittin part

--- drama-reader.js
+++ drama-reader.js
@@ -11,12 +11,79 @@ DramaReader.prototype = {
   wordIdx: null,
   input: null,
   words: null,
+  actions:null,
   isRunning: false,
   timers: [],
 
   setInput: function(input) {
     this.input = input;
+    this.actions = {};
 
+    var paragraphRegex = /(?:<p>)([\s\S]*?)(?:<\/p>)/g;
+    var paragraph = paragraphRegex.exec(input);
+    var inputParagraphs = [];
+    
+    while (paragraph != null)
+    {
+      inputParagraphs.push(paragraph[1]);
+      paragraph = paragraphRegex.exec(input);
+    }
+
+    console.log(inputParagraphs);
+
+    //for each paragraph, extract the data about the hyperlink words and where they should go
+
+    var allWords = [];
+    var wordIndex = 0;
+    var firstWordRegex = /^(.+?)(?:[\s]|$)([\s\S]*)/;
+    var isMarkupStartRegex = /<a/;
+    var isPassageLinkRegex = /^(?:<a).*(?:data-passage=")(.*?)(?:">)(.*?)<\/a>([\s\S]*)/;
+
+    for (paragraphIdx in inputParagraphs)
+    {
+      //let's do a cautious split, one "word" at a time. This should give us the capability to react to certain opening markup
+      var firstWord = firstWordRegex.exec(inputParagraphs[paragraphIdx]);
+
+      while (firstWord != null)
+        {
+          if (firstWord[1].match(isMarkupStartRegex) == null)
+          {
+            //it's just a word.
+            allWords.push(firstWord[1]);
+            wordIndex++;
+            firstWord = firstWordRegex.exec(firstWord[2]);
+          }
+          else
+          {
+            console.log("markup found! do something!");
+            //get the type of markup
+            if(firstWord[0].match(isPassageLinkRegex) != null)
+            {
+              var passageLinkData = isPassageLinkRegex.exec(firstWord[0]);
+              var linkWords = passageLinkData[2];
+              var linkPassage = passageLinkData[1];
+
+              var linkWord = firstWordRegex.exec(linkWords);
+              while (linkWord != null)
+              {
+                allWords.push(linkWord[1]);
+                wordIndex++;
+                this.actions[wordIndex] = {action:"showPassage", value:linkPassage};
+                linkWord = firstWordRegex.exec(linkWord[2]);
+              }
+              firstWord = firstWordRegex.exec(passageLinkData[3]); 
+              //TODO current sample fails here because the string ends up starting with that nasty newline character :(( )
+            }
+          }
+        }
+        console.log(allWords);
+        console.log(wordIndex);
+    }
+    
+
+
+    //NOTE: "Split on all spaces" is too simplistic for my needs.  I need to work on this particular part of the algorithm to support markup
+    // The rest of the processing should only be to adjust the clean, displayable text to the reader
     // Split on spaces
     var allWords = input.split(/\s+/);
 
